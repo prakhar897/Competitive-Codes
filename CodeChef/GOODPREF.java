ad99fe4049e269a6e657f9dd16e77756e6a056b3
Prakhar Gupta <prakhar897@gmail.com>
2018-04-18 10:56:03 +0530

    goodpref

---
+++ CodeChef/GOODPREF.java
@@ -0,0 +1,205 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author prakhar897
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                String str = in.nextString();
+                int m = str.length();
+                int n = in.nextInt();
+                int a[] = new int[m];
+                int b[] = new int[m];
+                long count = 0;
+                int i, av = 0, bv = 0, tc = 0, j = 0, tcp = 0;
+
+                for (i = 0; i < m; i++) {
+                    char ch = str.charAt(i);
+                    if (i == 0) {
+                        if (ch == 'a') {
+                            a[0] = 1;
+                            b[0] = 0;
+                        } else {
+                            a[0] = 0;
+                            b[0] = 1;
+                        }
+                    } else {
+                        if (ch == 'a') {
+                            a[i] = a[i - 1] + 1;
+                            b[i] = b[i - 1];
+                        } else {
+                            a[i] = a[i - 1];
+                            b[i] = b[i - 1] + 1;
+                        }
+                    }
+
+                    if (a[i] > b[i])
+                        count++;
+                }
+
+                av = a[m - 1];
+                bv = b[m - 1];
+                n = n - 1;
+                for (j = 0; j < n; j++) {
+                    tcp = tc;
+                    tc = 0;
+                    for (i = 0; i < m; i++) {
+                        a[i] += av;
+                        b[i] += bv;
+
+                        if (a[i] > b[i]) {
+                            tc++;
+                        }
+                    }
+                    count += tc;
+                    if (tc == 0)
+                        break;
+
+                    else if (tc == m) {
+                        count += (long) m * (n - 1 - j);
+                        break;
+                    } else if (tcp == tc) {
+                        count += (long) tc * (n - 1 - j);
+                        break;
+                    }
+
+                }
+                out.println(count);
+            }
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
