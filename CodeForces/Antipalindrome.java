310503c8e86c54bbf3f4507687ff2a172e1e49ae
Prakhar Gupta <prakhar897@gmail.com>
2018-05-28 03:03:13 +0530

    ap

---
+++ CodeForces/Antipalindrome.java
@@ -0,0 +1,151 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author prakhar897
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskG solver = new TaskG();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskG {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            String s = in.nextString();
+            int ans, flag = 0, i;
+            if (isPalindrome(s) == false)
+                ans = s.length();
+            else {
+                char c = s.charAt(0);
+                for (i = 1; i < s.length(); i++) {
+                    if (c != s.charAt(i)) {
+                        flag++;
+                        break;
+                    }
+                }
+
+                if (flag == 0)
+                    ans = 0;
+                else
+                    ans = s.length() - 1;
+            }
+
+            out.println(ans);
+        }
+
+        boolean isPalindrome(String s) {
+            int n = s.length();
+
+            for (int i = 0; i < (n / 2); ++i) {
+                if (s.charAt(i) != s.charAt(n - i - 1)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
