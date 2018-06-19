4fd4f7fb71933e847645649e802393cc9c471024
Prakhar Gupta <prakhar897@gmail.com>
2018-06-19 08:51:34 +0530

    992C

---
+++ CodeForces/992C - Nastya and a Wardrobe.java
@@ -0,0 +1,163 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author prakhar897
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            long mod = (long) 1e9 + 7;
+            long x = in.nextLong();
+            long k = in.nextLong() % (mod - 1);
+            if (x == 0) {
+                out.println(0);
+                return;
+            }
+            x %= mod;
+
+            long a = power(2, k, mod);
+            long b = power(2, k + 1, mod);
+            long d = (b * x) % mod;
+            long c = d + mod - a + 1;
+            c %= mod;
+            out.println(c);
+
+//        long ans = power(2, (int)((k+1) % (mod-1)), mod);
+//        ans = (1L*ans * x) % mod;
+//        ans = ans + mod + 1 - power(2, (int)((k) % (mod-1)), mod);
+//        ans %= mod;
+//        out.println(ans);
+        }
+
+        static long power(long x, long y, long p) {
+            long res = 1;
+            x = x % p;
+            while (y > 0) {
+                if ((y & 1) == 1)
+                    res = (res * x) % p;
+                y = y >> 1;
+                x = (x * x) % p;
+            }
+            return res;
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
