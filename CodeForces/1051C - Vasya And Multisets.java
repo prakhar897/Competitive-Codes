23bf9da5cc9f05d3b87fb9bf0a05c004de1be4cc
Prakhar Gupta <prakhar897@gmail.com>
2018-09-21 19:02:40 +0530

    1051C

---
+++ CodeForces/1051C - Vasya And Multisets.java
@@ -0,0 +1,196 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task51C solver = new Task51C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task51C {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int s[] = new int[101];
+            int arr[] = new int[n];
+            StringBuilder str = new StringBuilder();
+            int i, ms = 0, gv = -1, flag = 0;
+            for (i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+                s[arr[i]]++;
+            }
+
+            for (i = 0; i < 101; i++) {
+                if (s[i] == 1) {
+                    ms++;
+                }
+                if (s[i] >= 3) {
+                    gv = i;
+                }
+            }
+
+            if (ms % 2 == 0) {
+                int u = ms / 2;
+                for (i = 0; i < n; i++) {
+                    if (s[arr[i]] == 1) {
+                        if (u > 0) {
+                            str.append("A");
+                            u--;
+                        } else {
+                            str.append("B");
+                        }
+                    } else {
+                        str.append("A");
+                    }
+                }
+
+                out.println("YES");
+                out.println(str);
+            } else {
+                if (gv == -1) {
+                    out.println("NO");
+                    return;
+                } else {
+                    int u = ms / 2;
+                    for (i = 0; i < n; i++) {
+                        if (s[arr[i]] == 1) {
+                            if (u > 0) {
+                                str.append("A");
+                                u--;
+                            } else {
+                                str.append("B");
+                            }
+                        } else if (arr[i] == gv && flag == 0) {
+                            flag = 1;
+                            str.append("A");
+                        } else {
+                            str.append("B");
+                        }
+                    }
+
+                    out.println("YES");
+                    out.println(str);
+                }
+            }
+
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
