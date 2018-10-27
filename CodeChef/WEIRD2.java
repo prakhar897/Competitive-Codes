26c6900fa8ced3f3ce655f3e46ab16af349dee89
Prakhar Gupta <prakhar897@gmail.com>
2018-10-28 01:50:48 +0530

    WEIRD2

---
+++ CodeChef/WEIRD2.java
@@ -0,0 +1,156 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.io.BufferedWriter;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskOct3 solver = new TaskOct3();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskOct3 {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                int n = in.nextInt();
+                int i;
+                long ans = 0;
+                int arr[] = new int[n];
+                HashMap<Integer, Integer> hm = new HashMap<>();
+
+                for (i = 0; i < n; i++) {
+                    arr[i] = in.nextInt();
+                    hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+                }
+
+                Iterator it = hm.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry p = (Map.Entry) it.next();
+                    int x = (int) p.getKey();
+                    int y = (int) p.getValue();
+                    for (i = 1; i <= y; i++) {
+                        int z = hm.getOrDefault(i, 0);
+                        if (z >= x)
+                            ans++;
+                    }
+                }
+
+                out.println(ans);
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+}
+
