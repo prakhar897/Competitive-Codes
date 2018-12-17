a87952263b401c069600a675a828e525377496c0
Prakhar Gupta <prakhar897@gmail.com>
2018-12-17 20:05:27 +0530

    1084C

---
+++ CodeForces/1084C - The Fair Nut And The String.java
@@ -0,0 +1,151 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Vector;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task526C solver = new Task526C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task526C {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            String s = in.nextString();
+            Vector<Long> v = new Vector<>();
+            int i, n = s.length();
+            long ans = 1,cou=0;
+            long mod = (int)1e9+7;
+
+            for (i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                if (c == 'a')
+                    cou++;
+                else if (c == 'b' && cou != 0) {
+                    v.add(cou);
+                    cou = 0;
+                }
+            }
+
+            if (cou != 0)
+                v.add(cou);
+
+            for (long sa : v) {
+                sa++;
+                ans *= sa;
+                ans %= mod;
+            }
+            
+            //System.out.println(v);
+
+            ans--;
+            //ans += mod;
+            ans %= mod;
+            if(ans<0)
+                ans += mod;
+            //out.println(a);
+            out.println(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
