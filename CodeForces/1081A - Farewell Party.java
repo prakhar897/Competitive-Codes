7a4809cd19aeca2503ec571fc9e46b22e59f5a24
Prakhar Gupta <prakhar897@gmail.com>
2018-12-18 19:41:05 +0530

    1081A

---
+++ CodeForces/1081A - Farewell Party.java
@@ -0,0 +1,205 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Comparator;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskAvitoB solver = new TaskAvitoB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskAvitoB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            Node arr[] = new Node[n];
+            int ans[] = new int[n];
+            int i, v = 0, t = 0, cou = 0;
+
+            for (i = 0; i < n; i++) {
+                int a = n - in.nextInt();
+                arr[i] = new Node(i, a);
+            }
+
+            Arrays.sort(arr, new sort());
+
+        /*for(i=0;i<n;i++)
+        {
+            out.println(arr[i].key + " " + arr[i].value);
+        }*/
+
+            for (i = 0; i < n; i++) {
+                if (cou == 0) {
+                    t = (int) arr[i].value;
+                    cou = t;
+                    v++;
+                }
+                if (cou > 0) {
+                    int key = (int) arr[i].key;
+                    int val = (int) arr[i].value;
+                    ans[key] = v;
+                    cou--;
+                    if (t != val) {
+                        out.println("Impossible");
+                        return;
+                    }
+                }
+            }
+
+            if (cou != 0) {
+                out.println("Impossible");
+                return;
+            }
+
+            out.println("Possible");
+            out.print(ans);
+        }
+
+        class Node {
+            int key;
+            int value;
+
+            public Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+
+        }
+
+        class sort implements Comparator<Node> {
+            public int compare(Node a, Node b) {
+                return (int) a.value - (int) b.value;
+            }
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void print(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(array[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
