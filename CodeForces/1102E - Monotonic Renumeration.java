7628683860094626595b0a2b2e8bf88c2cb2e114
Prakhar Gupta <prakhar897@gmail.com>
2019-01-10 16:54:35 +0530

    1102E

---
+++ CodeForces/1102E - Monotonic Renumeration.java
@@ -0,0 +1,165 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.io.BufferedWriter;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task531E solver = new Task531E();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task531E {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            int c[] = new int[n];
+            HashMap<Integer, Integer> hm = new HashMap<>();
+            int i;
+
+            for (i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+                if (!hm.containsKey(arr[i])) {
+                    c[i] = 1;
+                }
+                hm.put(arr[i], i);
+            }
+
+            Iterator it = hm.entrySet().iterator();
+
+            while (it.hasNext()) {
+                HashMap.Entry p = (HashMap.Entry) it.next();
+                //out.println(p.getKey()+" " + p.getValue());
+                c[(int) p.getValue()]--;
+            }
+
+            //out.println(c);
+
+            int mod = 998244353;
+            int tot = c[0];
+            long ans = 1;
+
+            for (i = 1; i < n; i++) {
+                if (tot == 0) {
+                    ans = (ans * 2) % mod;
+                }
+                tot += c[i];
+            }
+
+            out.println(ans);
+
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+}
+
