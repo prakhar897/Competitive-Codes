f071e42942a7fd244531be16ef56f22cfa9d916f
Prakhar Gupta <prakhar897@gmail.com>
2019-01-17 15:56:05 +0530

    MINEAT

---
+++ CodeChef/MINEAT.java
@@ -0,0 +1,196 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        MINEAT solver = new MINEAT();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class MINEAT {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            outer:
+            while (test-- > 0) {
+                int n = in.nextInt();
+                long h = in.nextInt();
+                long arr[] = in.nextLongArray(n);
+                //out.println(arr);
+
+                Arrays.sort(arr);
+                long low = 1;
+                long high = arr[n - 1];
+
+                if (n == 1) {
+                    long a = arr[0] / h;
+                    if (arr[0] % h != 0)
+                        a++;
+                    out.println(a);
+                    continue outer;
+                }
+
+                while (high > low) {
+                    long mid = (high + low) / 2;
+                    //out.println(mid);
+                    long val = check(arr, n, mid, h);
+                    // out.println(val);
+                    if (val <= h) {
+                        high = mid;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+                out.println(low);
+            }
+        }
+
+        long check(long arr[], int n, long mid, long h) {
+            long ans = 0;
+            for (int i = 0; i < n; i++) {
+                ans += (long) arr[i] / mid;
+                if (arr[i] % mid != 0)
+                    ans++;
+            }
+            return ans;
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; ++i) array[i] = nextLong();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
