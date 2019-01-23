3510fdb0eb6dea424a31dcbb69e40d2c9f7d5823
Prakhar Gupta <prakhar897@gmail.com>
2019-01-23 16:13:31 +0530

    1103B

---
+++ CodeForces/1103B - Game With Modulo.java
@@ -0,0 +1,194 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task534D solver = new Task534D();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task534D {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int mid = 0;
+            while (true) {
+                String s = in.nextString();
+                if (!s.equals("start")) {
+                    return;
+                } else {
+                    out.println("? 0 1");
+                    out.flush();
+                    char t = in.nextCharacter();
+                    if (t == 'x') {
+                        out.println("! 1");
+                        out.flush();
+                        continue;
+                    } else if (t == 'e') {
+                        return;
+                    } else {
+                        int x = 0;
+                        int y = 1;
+                        while (t != 'x') {
+                            x *= 2;
+                            if(x==0)
+                                x=1;
+                            y *= 2;
+                            out.println("? " + x + " " + y);
+                            out.flush();
+                            t = in.nextCharacter();
+                        }
+                        //out.println("!! " + x + " " + y);
+                        //out.flush();
+
+                        // out.println("? " + (x + 1) + " " + y);
+                        // out.flush();
+                        if(y>(int) 1e9)
+                        {
+                            y = (int) 1e9;
+                        }
+
+                        while (y-x>1) {
+                            mid = (x + y) / 2;
+                            out.println("? " + x + " " + mid);
+                            out.flush();
+                            t = in.nextCharacter();
+                            
+                            if (t == 'x') {
+                                y = mid;  
+                            } else if (t == 'y') {
+                                x = mid;
+                            }
+                        }
+
+                        out.println("! " + (x+1));
+                        out.flush();
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public char nextCharacter() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            return (char) c;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
