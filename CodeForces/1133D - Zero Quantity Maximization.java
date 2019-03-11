3f97d8dc9535a1db3cae9405075ff0ac735ed2e5
Prakhar Gupta <prakhar897@gmail.com>
2019-03-12 04:16:16 +0530

    1133D

---
+++ CodeForces/1133D - Zero Quantity Maximization.java
@@ -0,0 +1,223 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.io.BufferedWriter;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task544D solver = new Task544D();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task544D {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int a[] = in.nextIntArray(n);
+            int b[] = in.nextIntArray(n);
+            int i, ans = 0, zab = 0, zero = 0;
+            HashMap<Pair<Integer, Integer>, Integer> hm = new HashMap<>();
+
+            for (i = 0; i < n; i++) {
+                if (a[i] == 0 && b[i] == 0)
+                    zab++;
+                else if (a[i] == 0) {
+
+                } else if (b[i] == 0) {
+                    zero++;
+                } else {
+                    int gcd = IntegerUtils.gcd(Math.abs(b[i]), Math.abs(a[i]));
+                    int bg = b[i] / gcd;
+                    int ag = a[i] / gcd;
+                    if (bg < 0) {
+                        ag *= -1;
+                        bg *= -1;
+                    }
+                    Pair t = new Pair(ag, bg);
+                    hm.put(t, hm.getOrDefault(t, 0) + 1);
+                }
+            }
+            //out.println(hm);
+
+            Iterator it = hm.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry p = (Map.Entry) it.next();
+                int v = (int) p.getValue();
+                ans = Math.max(ans, v);
+            }
+            ans += zab;
+            ans = Math.max(zero + zab, ans);
+            out.println(ans);
+        }
+
+    }
+
+    static class Pair<A, B> {
+        A first;
+        B second;
+
+        public Pair(A first, B second) {
+            super();
+            this.first = first;
+            this.second = second;
+        }
+
+        public int hashCode() {
+            int hashFirst = first != null ? first.hashCode() : 0;
+            int hashSecond = second != null ? second.hashCode() : 0;
+
+            return (hashFirst + hashSecond) * hashSecond + hashFirst;
+        }
+
+        public boolean equals(Object other) {
+            if (other instanceof Pair) {
+                Pair otherPair = (Pair) other;
+                return
+                        ((this.first == otherPair.first ||
+                                (this.first != null && otherPair.first != null &&
+                                        this.first.equals(otherPair.first))) &&
+                                (this.second == otherPair.second ||
+                                        (this.second != null && otherPair.second != null &&
+                                                this.second.equals(otherPair.second))));
+            }
+
+            return false;
+        }
+
+        public String toString() {
+            return "(" + first + ", " + second + ")";
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class IntegerUtils {
+        public static int gcd(int a, int b) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            while (b != 0) {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+    }
+}
+
