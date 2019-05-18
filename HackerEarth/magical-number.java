1d707cb3453052bd7fc02342104bd5b73ddc2bd9
Prakhar Gupta <prakhar897@gmail.com>
2019-05-19 01:19:37 +0530

    magical-number

---
+++ HackerEarth/magical-number.java
@@ -0,0 +1,207 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        BONUS solver = new BONUS();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class BONUS {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                int n = in.nextInt();
+                long k = in.nextLong();
+                long ans[] = new long[n];
+                ans[n - 1] = k;
+                int i, j;
+                ArrayList<Integer> al = new ArrayList<>();
+                String str = Long.toBinaryString(k);
+
+                for (i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) == '1')
+                        al.add(i);
+                }
+
+                long powsetsize = (long) Math.pow(2, al.size());
+                if (powsetsize < n) {
+                    out.println(-1);
+                    continue;
+                }
+
+                for (i = 0; i < n - 1; i++) {
+                    int ar[] = new int[str.length()];
+                    for (j = 0; j < al.size(); j++) {
+                        if ((i & (1 << j)) > 0)
+                            ar[al.get(j)] = 1;
+                    }
+
+                    StringBuilder sb = new StringBuilder();
+                    for (j = 0; j < str.length(); j++) {
+                        if (ar[j] == 0)
+                            sb.append('0');
+                        else
+                            sb.append('1');
+                    }
+
+                    ans[i] = parseLong(sb.toString(), 2);
+                }
+
+                out.println(ans);
+            }
+        }
+
+        long parseLong(String s, int base) {
+            return new BigInteger(s, base).longValue();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(long[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(array[i]);
+            }
+        }
+
+        public void println(long[] array) {
+            print(array);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
