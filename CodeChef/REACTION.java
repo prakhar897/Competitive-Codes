bca4df3d0b87e6c6417d1cc06a65e0acdf9170b6
Prakhar Gupta <prakhar897@gmail.com>
2019-05-20 01:19:44 +0530

    REACTION

---
+++ CodeChef/REACTION.java
@@ -0,0 +1,169 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        ChefB solver = new ChefB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class ChefB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                int r = in.nextInt();
+                int c = in.nextInt();
+                int i, j, flag = 0;
+                int mat[][] = new int[r][c];
+                for (i = 0; i < r; i++)
+                    for (j = 0; j < c; j++)
+                        mat[i][j] = in.nextInt();
+
+                out:
+                for (i = 0; i < r; i++) {
+                    for (j = 0; j < c; j++) {
+                        int val = 0;
+                        if (i + 1 < r)
+                            val++;
+                        if (i - 1 >= 0)
+                            val++;
+                        if (j + 1 < c)
+                            val++;
+                        if (j - 1 >= 0)
+                            val++;
+
+                        if (mat[i][j] >= val) {
+                            flag = 1;
+                            break out;
+                        }
+                    }
+                }
+
+                if (flag == 0)
+                    out.println("Stable");
+                else
+                    out.println("Unstable");
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
