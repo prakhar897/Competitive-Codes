6cdde90bc6623c351a7fa8d345246efadbb0d97a
Prakhar Gupta <prakhar897@gmail.com>
2019-06-14 23:25:13 +0530

    MaxMul

---
+++ HackerEarth/MaxMul.java
@@ -0,0 +1,160 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        MaxMul solver = new MaxMul();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class MaxMul {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                int n = in.nextInt();
+                int arr[] = in.nextIntArray(n);
+                int max = Integer.MIN_VALUE, smax = Integer.MIN_VALUE, min = Integer.MAX_VALUE, smin = Integer.MAX_VALUE;
+                for (int i = 0; i < n; i++) {
+                    if (arr[i] > max) {
+                        smax = max;
+                        max = arr[i];
+                    } else if (arr[i] <= max && arr[i] > smax) {
+                        smax = arr[i];
+                    }
+
+                    if (arr[i] < min) {
+                        smin = min;
+                        min = arr[i];
+                    } else if (arr[i] >= min && arr[i] < smin)
+                        smin = arr[i];
+                }
+
+                long ans = Math.max(1l * max * smax, 1l * min * smin);
+                if (n == 1)
+                    out.println(arr[0]);
+                else
+                    out.println(ans);
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
