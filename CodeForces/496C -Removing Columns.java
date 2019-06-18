7f32dc55ca142ddbf6e97ca88a5961395898b6fe
Prakhar Gupta <prakhar897@gmail.com>
2019-06-19 01:21:51 +0530

    496C

---
+++ CodeForces/496C -Removing Columns.java
@@ -0,0 +1,201 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task496C solver = new Task496C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task496C {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int mat[][] = new int[n][m];
+            int always[] = new int[m];
+            int row[] = new int[n];
+            Arrays.fill(always, 1);
+            int i, j, count = 0, k;
+
+            for (i = 0; i < n; i++) {
+                String s = in.nextString();
+                for (j = 0; j < m; j++) {
+                    int a = s.charAt(j) - 'a';
+                    mat[i][j] = a;
+                }
+            }
+
+            //out.print(mat);
+
+            for (j = 0; j < m; j++) {
+                for (i = 1; i < n; i++) {
+                    if (mat[i][j] < mat[i - 1][j]) {
+                        always[j] = 0;
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            //out.println(always);
+
+            row[0] = 1;
+            for (j = 0; j < m; j++) {
+                if (always[j] == 1) {
+                    for (i = 1; i < n; i++) {
+                        if (mat[i][j] > mat[i - 1][j])
+                            row[i] = 1;
+                    }
+                } else {
+                    for (i = 1; i < n; i++) {
+                        if (row[i] == 1 || mat[i][j] >= mat[i - 1][j])
+                            continue;
+                        else
+                            break;
+                    }
+
+                    if (i == n) {
+                        always[j] = 1;
+                        count--;
+                        for (i = 1; i < n; i++) {
+                            if (mat[i][j] > mat[i - 1][j])
+                                row[i] = 1;
+                        }
+                    }
+                }
+                //out.println(row);
+            }
+
+            //out.println(always);
+            out.println(count);
+
+
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
