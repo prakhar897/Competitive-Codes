e792f1907553aad22c0881877a7295f79231c728
Prakhar Gupta <prakhar897@gmail.com>
2019-06-24 02:56:39 +0530

    SLUSH

---
+++ CodeChef/SLUSH.java
@@ -0,0 +1,191 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskChefC solver = new TaskChefC();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskChefC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                int n = in.nextInt();
+                int m = in.nextInt();
+                int arr[] = in.nextIntArray(m);
+                int ans[] = new int[n];
+                int count[] = new int[m];
+                Queue<Integer> q = new LinkedList<>();
+                int mat[][] = new int[n][3];
+                int i;
+
+                for (i = 0; i < n; i++) {
+                    mat[i][0] = in.nextInt() - 1;
+                    mat[i][1] = in.nextInt();
+                    mat[i][2] = in.nextInt();
+                    count[mat[i][0]]++;
+                }
+
+                for (i = 0; i < m; i++) {
+                    if (arr[i] > count[i]) {
+                        q.offer(i);
+                    }
+                }
+
+                long cost = 0;
+                for (i = 0; i < n; i++) {
+                    if (arr[mat[i][0]] > 0) {
+                        ans[i] = mat[i][0] + 1;
+                        cost += mat[i][1];
+                        arr[mat[i][0]]--;
+                        count[mat[i][0]]--;
+                    } else {
+                        int k = q.peek();
+                        ans[i] = k + 1;
+                        cost += mat[i][2];
+                        arr[k]--;
+                        count[mat[i][0]]--;
+                        if (arr[k] == count[k])
+                            q.poll();
+                    }
+                }
+
+                out.println(cost);
+                out.println(ans);
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(array[i]);
+            }
+        }
+
+        public void println(int[] array) {
+            print(array);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
