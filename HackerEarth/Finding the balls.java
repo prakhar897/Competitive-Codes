0171934efe1958cc9df4d59f72b68c5e14f9aae5
Prakhar Gupta <prakhar897@gmail.com>
2019-07-17 14:59:18 +0530

    balls

---
+++ HackerEarth/Finding the balls.java
@@ -0,0 +1,197 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Taskballs solver = new Taskballs();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Taskballs {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int arr[] = in.nextIntArray(n);
+            HashSet<Integer> hs = new HashSet<>();
+            Arrays.sort(arr);
+            int arr2[] = new int[n];
+            int i;
+
+            for (i = 0; i < n; i++)
+                hs.add(arr[i]);
+
+            for (i = 1; i < n; i++) {
+                arr2[i] = arr[i] - arr[i - 1];
+            }
+            arr2[0] = 360 - (arr[n - 1] - arr[0]);
+            //out.println(arr2);
+
+            int gcd = arr2[0];
+            for (i = 1; i < n; i++) {
+                gcd = IntegerUtils.gcd(gcd, arr2[i]);
+            }
+            //out.println(gcd);
+
+            ArrayList<Integer> ans = new ArrayList<>();
+            int start = arr[0] + gcd;
+            start %= 360;
+
+            while (start != arr[0]) {
+                if (!hs.contains(start))
+                    ans.add(start);
+                start += gcd;
+                start %= 360;
+            }
+
+            out.println(ans.size());
+            Collections.sort(ans);
+            for (int x : ans) {
+                out.print(x + " ");
+            }
+
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class IntegerUtils {
+        public static int gcd(int a, int b) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            while (b != 0) {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
