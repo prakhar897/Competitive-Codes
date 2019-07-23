c933517669b5fab911dc815523cbef2f32127e76
Prakhar Gupta <prakhar897@gmail.com>
2019-07-23 20:04:13 +0530

    1197B

---
+++ CodeForces/1197B - Pillars.java
@@ -0,0 +1,192 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.TreeMap;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task69B solver = new Task69B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task69B {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt();
+            int arr[] = in.nextIntArray(n);
+            TreeMap<Integer, Integer> tm = new TreeMap<>();
+            HashMap<Integer, Integer> hm = new HashMap<>();
+            int i;
+
+            for (i = 0; i < n; i++) {
+                hm.put(arr[i], i);
+                tm.put(i, arr[i]);
+            }
+
+            tm.put(-1, -1);
+            tm.put(Integer.MAX_VALUE, n + 1);
+
+            int count = n;
+            while (count != 1) {
+                //System.out.println(hm);
+                //System.out.println(tm);
+                int p = hm.get(count);
+                int a = tm.lowerKey(p);
+                if (tm.get(a) + 1 == count) {
+                    int val = tm.get(a);
+                    int key = p;
+                    hm.remove(tm.get(a));
+                    hm.remove(tm.get(p));
+                    tm.remove(a);
+                    tm.remove(p);
+                    tm.put(key, val);
+                    hm.put(val, key);
+                    count--;
+                    continue;
+                }
+                a = tm.higherKey(p);
+                if (tm.get(a) + 1 == count) {
+                    int val = tm.get(a);
+                    int key = p;
+                    hm.remove(tm.get(a));
+                    hm.remove(tm.get(p));
+                    tm.remove(a);
+                    tm.remove(p);
+                    tm.put(key, val);
+                    hm.put(val, key);
+                    count--;
+                    continue;
+                }
+
+                out.println("NO");
+                return;
+            }
+            out.println("YES");
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
