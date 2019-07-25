34553269b3341818784e7e7efb25659f1a39bca4
Prakhar Gupta <prakhar897@gmail.com>
2019-07-25 19:51:35 +0530

    1196A

--- CodeForces/1196A - Three Piles of Candies.java
+++ CodeForces/1196A - Three Piles of Candies.java
@@ -1,9 +1,9 @@
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.io.BufferedWriter;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
@@ -21,155 +21,22 @@ public class Main {
         OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
-        Task575E solver = new Task575E();
+        Task575A solver = new Task575A();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class Task575E {
+    static class Task575A {
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int q = in.nextInt();
-            int mat[][] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-            loop:
             while (q-- > 0) {
-                int b = in.nextInt();
-                int w = in.nextInt();
-                int a = (int) 100;
-                HashSet<Pair> white = new HashSet<>();
-                HashSet<Pair> black = new HashSet<>();
-
-                if (w > b) {
-                    long max = 4 + (b - 1) * 3;
-                    if (w > max) {
-                        out.println("NO");
-                        continue loop;
-                    }
-                } else {
-                    long max = 4 + (w - 1) * 3;
-                    if (b > max) {
-                        out.println("NO");
-                        continue loop;
-                    }
-                }
-
-                black.add(new Pair(a / 2, (a / 2) + 1));
-                while (white.size() < w || black.size() < b) {
-                    for (Pair x : black) {
-                        int f = x.f;
-                        int s = x.s;
-
-                        for (int j = 0; j < 4; j++) {
-                            int curf = f + mat[j][0];
-                            int curs = s + mat[j][1];
-                            white.add(new Pair(curf, curs));
-                        }
-                    }
-
-                    for (Pair x : white) {
-                        int f = x.f;
-                        int s = x.s;
-
-                        for (int j = 0; j < 4; j++) {
-                            int curf = f + mat[j][0];
-                            int curs = s + mat[j][1];
-                            black.add(new Pair(curf, curs));
-                        }
-                    }
-                }
-
-                out.println("YES");
-                int count = 0;
-                for (Pair x : white) {
-                    out.println(x.f + " " + x.s);
-                    count++;
-                    if (count == w)
-                        break;
-                }
-
-                count = 0;
-                for (Pair x : black) {
-                    out.println(x.f + " " + x.s);
-                    count++;
-                    if (count == b)
-                        break;
-                }
+                long arr[] = in.nextLongArray(3);
+                Arrays.sort(arr);
+                long diff = arr[1] - arr[0];
+                arr[2] -= diff;
+                long ans = arr[1] + (arr[2] / 2);
+                out.println(ans);
             }
-        }
-
-        class Pair implements Comparable {
-            int f;
-            int s;
-
-            Pair(int fi, int se) {
-                f = fi;
-                s = se;
-            }
-
-            public int compareTo(Object o) {
-                Pair pr = (Pair) o;
-                if (s > pr.s)
-                    return 1;
-                if (s == pr.s) {
-                    if (f > pr.f)
-                        return 1;
-                    else
-                        return -1;
-                } else
-                    return -1;
-            }
-
-            public boolean equals(Object o) {
-                Pair ob = (Pair) o;
-                int ff;
-                int ss;
-                if (o != null) {
-                    ff = ob.f;
-                    ss = ob.s;
-                    if ((ff == this.f) && (ss == this.s))
-                        return true;
-                }
-                return false;
-            }
-
-            public int hashCode() {
-                return (this.f + " " + this.s).hashCode();
-            }
-
-            public String toString() {
-                return this.f + " " + this.s;
-            }
-
-        }
-
-    }
-
-    static class OutputWriter {
-        private final PrintWriter writer;
-
-        public OutputWriter(OutputStream outputStream) {
-            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
-        }
-
-        public OutputWriter(Writer writer) {
-            this.writer = new PrintWriter(writer);
-        }
-
-        public void print(Object... objects) {
-            for (int i = 0; i < objects.length; i++) {
-                if (i != 0) {
-                    writer.print(' ');
-                }
-                writer.print(objects[i]);
-            }
-        }
-
-        public void println(Object... objects) {
-            print(objects);
-            writer.println();
-        }
-
-        public void close() {
-            writer.close();
         }
 
     }
@@ -225,6 +92,28 @@ public class Main {
             return res * sgn;
         }
 
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
         public boolean isSpaceChar(int c) {
             if (filter != null) {
                 return filter.isSpaceChar(c);
@@ -236,9 +125,36 @@ public class Main {
             return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
         }
 
+        public long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; ++i) array[i] = nextLong();
+            return array;
+        }
+
         public interface SpaceCharFilter {
             public boolean isSpaceChar(int ch);
 
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
         }
 
     }
