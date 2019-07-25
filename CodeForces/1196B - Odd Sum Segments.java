b00b1f1cb8845c6d16f61c6ada1352de7829b9fd
Prakhar Gupta <prakhar897@gmail.com>
2019-07-25 19:52:40 +0530

    1196B

---
+++ CodeForces/1196B - Odd Sum Segments.java
@@ -0,0 +1,189 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task575B solver = new Task575B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task575B {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int q = in.nextInt();
+            while (q-- > 0) {
+                int n = in.nextInt();
+                int k = in.nextInt();
+                int i, count = 0, j = 0;
+                int arr[] = new int[n];
+                int ans[] = new int[k];
+                int ans2[] = new int[k];
+
+                for (i = 0; i < n; i++) {
+                    int a = in.nextInt();
+                    arr[i] = a % 2;
+                    if (arr[i] == 1)
+                        count++;
+                }
+
+                if (count < k || count % 2 != k % 2) {
+                    out.println("NO");
+                    continue;
+                }
+
+                for (i = 0; i < n; i++) {
+                    if (arr[i] == 1) {
+                        ans[j] = i + 1;
+                        j++;
+
+                        if (j == k)
+                            break;
+                    }
+                }
+
+                for (i = 0; i < k - 1; i++) {
+                    ans2[i] = ans[i + 1] - 1;
+                }
+                ans2[k - 1] = n;
+
+                out.println("YES");
+                out.println(ans2);
+
+
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void print(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(array[i]);
+            }
+        }
+
+        public void println(int[] array) {
+            print(array);
+            writer.println();
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
