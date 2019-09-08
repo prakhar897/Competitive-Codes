0da154225e85c0ee2d7a62e50b50307533a9314d
Prakhar Gupta <prakhar897@gmail.com>
2019-09-08 23:02:20 +0530

    1217C

---
+++ CodeForces/1217C - The Number Of Good Substrings.java
@@ -0,0 +1,182 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+ 
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task72C solver = new Task72C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+ 
+    static class Task72C {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int test = in.nextInt();
+            while (test-- > 0) {
+                char arr[] = in.nextString().toCharArray();
+                int n = arr.length;
+                int front[] = new int[n];
+                int i, cur = 0;
+                long ans = 0;
+ 
+                for (i = 0; i < n; i++) {
+                    if (arr[i] == '1') {
+                        front[i] = cur;
+                        cur = i + 1;
+                    }
+                }
+ 
+                //out.println(front);
+ 
+                for (i = 0; i < n; i++) {
+                    if (arr[i] == '1') {
+                        ans += 1;
+                        long val = 1;
+                        long len = 1;
+                        long u = i - front[i];
+                        for (int j = i + 1; j < n; j++) {
+                            val <<= 1;
+                            if (arr[j] == '1')
+                                val++;
+                            len++;
+                            long h = val - len;
+                            if (h > u)
+                                break;
+                            else {
+                                ans++;
+                                //out.println(i + " " + j);
+                            }
+ 
+                        }
+                    }
+                }
+ 
+                out.println(ans);
+ 
+ 
+            }
+        }
+ 
+    }
+ 
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+ 
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+ 
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+ 
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+ 
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+ 
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+ 
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+ 
+        }
+ 
+    }
+ 
+    static class OutputWriter {
+        private final PrintWriter writer;
+ 
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+ 
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+ 
+        public void close() {
+            writer.close();
+        }
+ 
+        public void println(long i) {
+            writer.println(i);
+        }
+ 
+    }
+}
