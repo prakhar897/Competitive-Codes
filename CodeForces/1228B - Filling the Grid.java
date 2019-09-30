9f78b31ceb6b10b152bd32f7f67cd4e792013e50
Prakhar Gupta <prakhar897@gmail.com>
2019-09-30 14:24:05 +0530

    1228B

---
+++ CodeForces/1228B - Filling the Grid.java
@@ -0,0 +1,202 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author prakhar897
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        BFillingTheGrid solver = new BFillingTheGrid();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class BFillingTheGrid {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int h = in.nextInt();
+            int w = in.nextInt();
+            int r[] = in.nextIntArray(h);
+            int c[] = in.nextIntArray(w);
+            int i, j;
+            int mat[][] = new int[h][w];
+
+            for (i = 0; i < h; i++) {
+                for (j = 0; j < r[i]; j++) {
+                    mat[i][j] = 1;
+                }
+
+                if (j < w)
+                    mat[i][j] = 2;
+            }
+
+            for (i = 0; i < w; i++) {
+                for (j = 0; j < c[i]; j++) {
+                    if (mat[j][i] == 2) {
+                        out.println(0);
+                        return;
+                    }
+                    mat[j][i] = 1;
+                }
+
+                if (j < h) {
+                    if (mat[j][i] == 1) {
+                        out.println(0);
+                        return;
+                    }
+                    mat[j][i] = 2;
+                }
+
+            }
+
+            //out.println(mat);
+
+            int count = 0;
+            for (i = 0; i < h; i++) {
+                for (j = 0; j < w; j++) {
+                    if (mat[i][j] == 0)
+                        count++;
+                }
+            }
+
+            int mod = (int) 1e9 + 7;
+            long ans = IntegerUtils.power(2, count, mod);
+            out.println(ans);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i) array[i] = nextInt();
+            return array;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class IntegerUtils {
+        public static long power(long base, long exponent, long mod) {
+            if (base >= mod) {
+                base %= mod;
+            }
+            if (exponent == 0) {
+                return 1 % mod;
+            }
+            long result = power(base, exponent >> 1, mod);
+            result = result * result % mod;
+            if ((exponent & 1) != 0) {
+                result = result * base % mod;
+            }
+            return result;
+        }
+
+    }
+}
+
